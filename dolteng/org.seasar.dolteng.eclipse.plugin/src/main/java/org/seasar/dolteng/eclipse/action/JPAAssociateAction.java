4b8a74fcf03e3cd0ae34382a4028521d3ec0de4a
taichi <ryushi@gmail.com>
2006-07-26 08:42:10 +0000

    git-svn-id: https://www.seasar.org/svn/chura/trunk@33 6a3759fa-7269-4e70-8ae1-ab844f716fa1

---
+++ dolteng/org.seasar.dolteng.eclipse.plugin/src/main/java/org/seasar/dolteng/eclipse/action/JPAAssociateAction.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dolteng.eclipse.action;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.seasar.dolteng.eclipse.DoltengCore;
+
+/**
+ * @author taichi
+ * 
+ */
+public class JPAAssociateAction implements IEditorActionDelegate {
+
+    private IEditorPart targetEditor;
+
+    private ITextSelection selection;
+
+    /**
+     * 
+     */
+    public JPAAssociateAction() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction,
+     *      org.eclipse.ui.IEditorPart)
+     */
+    public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+        this.targetEditor = targetEditor;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction,
+     *      org.eclipse.jface.viewers.ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+        if (selection instanceof ITextSelection) {
+            this.selection = (ITextSelection) selection;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IAction action) {
+        try {
+            ICompilationUnit cu = getCompilationUnit();
+            if (this.selection != null && cu != null && cu.exists()) {
+                IJavaElement[] elems = cu.codeSelect(
+                        this.selection.getOffset(), this.selection.getLength());
+                if (elems != null && 0 < elems.length
+                        && elems[0] instanceof IField) {
+                    IField field = (IField) elems[0];
+                    System.out.println("source : " + field.getSource());
+                    System.out.println("offset : "
+                            + field.getSourceRange().getOffset());
+                    System.out.println("length : "
+                            + field.getSourceRange().getLength());
+                }
+            }
+        } catch (CoreException e) {
+            DoltengCore.log(e);
+        }
+    }
+
+    private ICompilationUnit getCompilationUnit() {
+        if (this.targetEditor == null) {
+            return null;
+        }
+        Object unit = this.targetEditor.getEditorInput().getAdapter(
+                IJavaElement.class);
+        if (unit instanceof ICompilationUnit) {
+            return (ICompilationUnit) unit;
+        }
+        return null;
+    }
+
+}
---
+++ dolteng/org.seasar.dolteng.eclipse.plugin/src/main/java/org/seasar/dolteng/eclipse/operation/AddJPAAssociationOperation.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.dolteng.eclipse.operation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IType;
+
+/**
+ * @author taichi
+ * 
+ */
+public class AddJPAAssociationOperation implements IWorkspaceRunnable {
+
+    public static class AnnotationElements {
+        public String targetEntity = void.class.getName();
+
+        public List cascade = new ArrayList();
+
+        public String fetch = "EAGER";
+
+        public boolean optional = true;
+
+        public String mappedBy = "";
+    }
+
+    private AnnotationElements elements;
+
+    public AddJPAAssociationOperation(ICompilationUnit rootAst, IType type,
+            IField target, AnnotationElements elements) {
+        super();
+        this.elements = elements;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.resources.IWorkspaceRunnable#run(org.eclipse.core.runtime.IProgressMonitor)
+     */
+    public void run(IProgressMonitor monitor) throws CoreException {
+    }
+
+}
