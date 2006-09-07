bf063ebd9517eeb7a05f25e10f9fdf2c3f1e32af
Yasuo Higa <higayasuo@gmail.com>
2006-09-07 10:22:34 +0000

    git-svn-id: https://www.seasar.org/svn/chura/trunk@176 6a3759fa-7269-4e70-8ae1-ab844f716fa1

--- chura-example/WEB-INF/src/examples/chura/web/add/AddInputPage.java
+++ chura-example/WEB-INF/src/examples/chura/web/add/AddInputPage.java
@@ -1,28 +1,28 @@
 package examples.chura.web.add;
 
+import org.seasar.teeda.extension.annotation.validator.Required;
+
 public class AddInputPage {
 
-	public static final String arg1_VALIDATOR = "#{'validator':'length','minimum':3}";
+	@Required
+	private Integer arg1;
 
-	public static final String arg2_VALIDATOR = "#{'validator':'length','minimum':3}";
+	@Required
+	private Integer arg2;
 
-	private int arg1;
-
-	private int arg2;
-
-	public int getArg1() {
+	public Integer getArg1() {
 		return arg1;
 	}
 
-	public void setArg1(int arg1) {
+	public void setArg1(Integer arg1) {
 		this.arg1 = arg1;
 	}
 
-	public int getArg2() {
+	public Integer getArg2() {
 		return arg2;
 	}
 
-	public void setArg2(int arg2) {
+	public void setArg2(Integer arg2) {
 		this.arg2 = arg2;
 	}
 }
--- chura-example/WEB-INF/src/examples/chura/web/add/AddResultPage.java
+++ chura-example/WEB-INF/src/examples/chura/web/add/AddResultPage.java
@@ -1,39 +1,39 @@
 package examples.chura.web.add;
 
 public class AddResultPage {
-    
-	private int arg1;
-    
-    private int arg2;
-    
-    private int result;
 
-    public int getArg1() {
-        return arg1;
-    }
+	private Integer arg1;
 
-    public void setArg1(int arg1) {
-        this.arg1 = arg1;
-    }
+	private Integer arg2;
 
-    public int getArg2() {
-        return arg2;
-    }
+	private Integer result;
 
-    public void setArg2(int arg2) {
-        this.arg2 = arg2;
-    }
+	public Integer getArg1() {
+		return arg1;
+	}
 
-    public int getResult() {
-        return result;
-    }
+	public void setArg1(Integer arg1) {
+		this.arg1 = arg1;
+	}
 
-    public void setResult(int result) {
-        this.result = result;
-    }
-    
-    public String initialize() {
-        result = arg1 + arg2;
-        return null;
-    }
+	public Integer getArg2() {
+		return arg2;
+	}
+
+	public void setArg2(Integer arg2) {
+		this.arg2 = arg2;
+	}
+
+	public Integer getResult() {
+		return result;
+	}
+
+	public void setResult(Integer result) {
+		this.result = result;
+	}
+
+	public String initialize() {
+		result = arg1 + arg2;
+		return null;
+	}
 }
