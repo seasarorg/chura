bf036b0b607feefd1800af6599ba4f5350da9937
Yasuo Higa <higayasuo@gmail.com>
2008-01-08 14:14:38 +0000

    Initial import.
    git-svn-id: https://www.seasar.org/svn/chura/trunk@778 6a3759fa-7269-4e70-8ae1-ab844f716fa1

---
+++ chura-tutorial/src/main/java/tutorial/chura/web/hello/HelloPage.java
@@ -0,0 +1,14 @@
+package tutorial.chura.web.hello;
+
+public class HelloPage {
+
+	public String name = "Seasar2";
+
+	public Class initialize() {
+		return null;
+	}
+
+	public Class prerender() {
+		return null;
+	}
+}
